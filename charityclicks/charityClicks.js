e15b21d8aff6cfa2e8c5f5bbcb9e5949393e38fb
David Morgantini <dmorgantini@gmail.com>
2011-12-04 09:24:00 +0000

    Added different sites

--- charityclicks/charityClicks.js
+++ charityclicks/charityClicks.js
@@ -1,42 +1,29 @@
 $(document).ready(function() {
-    $('a:contains("@ScottMuc")').each(function() {
-        var element = createImageDiv(this);
-        $(element).qtip({
-            content: '<a class="donation" href="http://www.google.com" target="_blank">Donate</a>' +
-                '&nbsp;|&nbsp;' +
-                '<a class="donation" href="http://www.google.com" target="_blank">View Charity Information</a>',
-            position: 'right',
-            hide: {fixed: true}
+
+    var names = {
+        1: "Empty Homes",
+        2: "Real Lettings",
+        3: "Shelter"
+    };
+
+    for (var id in names){
+        $('a:contains('+ names[id] +')').each(function() {
+            var element = $('<div/>').appendTo(this).attr("style", "display: inline-block; " +
+                "cursor: pointer; " +
+                "width: 16px; " +
+                "height: 16px;" +
+                "margin-left: 5px;" +
+                "background: url('" + chrome.extension.getURL("skin/fusion/16_16/plain/r5.png") + "')");
+
+            $(element).qtip({
+                content: '<a class="donation" href="http://www.google.com/search?q='+ names[id] +'" target="_blank">Donate Now</a>' +
+                    '&nbsp;|&nbsp;' +
+                    '<a class="donation" href="http://www.google.com/search?q='+ names[id] +'" target="_blank">View Charity Information</a>' +
+                    '<div style="font-weight: bold;">Text To Donate £5: 01234 567891</div>',
+                position: 'right',
+                hide: {fixed: true}
+            });
         });
-    });
-});
-
-function createImageDiv(link) {
-    try {
-        var elem = document.createElement("div");
-
-        if (elem) {
-            elem.setAttribute("style",
-                "display: inline-block; " +
-                    "cursor: pointer; " +
-                    "width: 16px; " +
-                    "height: 16px;" +
-                    "background: url('" + chrome.extension.getURL("skin/fusion/16_16/plain/r5.png") + "')");
-
-            elem.addEventListener("click", this.onclickdonateicon, false);
-
-            if (link.nextSibling) {
-                elem = link.parentNode.insertBefore(elem, link.nextSibling);
-            } else {
-                elem = link.parentNode.appendChild(elem);
-            }
-
-            elem.innerHTML = "&nbsp;";
-
-        }
-        return elem;
-    } catch (e) {
-        console.log("createImageDiv: failed with " + e + "\n");
-        return null;
     }
-}
+
+});
